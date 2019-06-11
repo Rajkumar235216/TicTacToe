cbc5b33b8d8f6e9a91998e85c8cc449d46b831a7
Mukesh <Mukesh@DESKTOP-J29D8QM>
2019-06-11 22:21:37 +0530

    Tic tac toe game by raj

--- src/main/java/main/TicTacToe.java
+++ src/main/java/main/TicTacToe.java
@@ -7,20 +7,122 @@ public static int[][] ticbox;
 	public static int win(int [][]tic) {
 		//check all rows
 		//loop through rows from 0 to 3 and check if all the 3 places have same marks
- 		
-		
+		//int r=0 ,c = 0,d =0;
+		int count;
+		for(int i = 0; i<3; i++)
+ 			{
+			   count = 0;
+ 				for(int j = 0; j<3; j++)
+ 	 			{
+ 	 				if (tic[i][j] == 1 && count <3)
+ 	 				{
+ 	 					count+= 1;
+ 	 				}
+ 	 				
+ 	 			}
+ 				if (count == 3)
+	 				{
+	 					return 1;
+	 				}
+	 				else
+	 					{
+	 					continue;}
+ 			}
+ 			
+  		for(int i = 0; i<3; i++)
+ 			{  count = 0;
+ 				for(int j = 0; j<3; j++)
+ 	 			{
+ 	 				if (tic[i][j] == 2 && count < 3)
+ 	 				{
+ 	 					count+= 1;
+ 	 				}
+ 	 				
+	 			}
+ 				if (count == 3)
+	 				{
+	 					return 2;
+	 				}
+	 				else
+ 					{continue;}
+ 			}
+			
+ 
 		//check all cols
 		//loop through columns from 0 to 3 and check if all the 3 places have same marks
- 		
+ 		for(int i = 0; i<3; i++)
+ 			{   count = 0;
+ 				for(int j = 0; j<3; j++)
+ 	 			{  
+ 	 				if (tic[j][i] == 1 && count < 3)
+ 	 				{
+ 	 					count += 1;
+ 	 				}
+ 	 				
+ 	 			}
+ 				if (count == 3)
+	 				{
+	 					return 1;
+	 				}
+	 			else
+	 				{continue;}
+ 			}
+ 		for(int i = 0; i<3; i++)
+ 			{  count = 0;
+ 				for(int j = 0; j<3; j++)
+ 	 			{
+ 	 				if (tic[j][i] == 2 && count < 3)
+ 	 				{
+ 	 					count += 1;
+ 	 				}
+ 	 			}
+ 				if (count == 3)
+	 				{
+	 					return 2;
+	 				}
+	 				else
+				{continue;}
+
+ 			}
+ 
+	
 		//check both diagonals 
-		
-		
+ 		{
+ 			count = 0;
+ 			for(int j = 0; j<3; j++)
+	 			{
+	 				if (tic[j][j] == 2)
+	 				{
+	 					count += 1;
+	 				}
+	 			}
+					
+ 			if (count == 3)
+				{
+					return 2;
+				}
+            count = 0;
+ 		
+ 			for(int j = 0; j<3; j++)
+	 			{
+	 				if (tic[j][j] == 1)
+	 				{
+	 					count += 1;
+	 				}
+	 			}		
+ 			if (count == 3)
+				{
+					return 1;
+				}
+			 
+			return 0;
+ 		}
 		
 		//write your code here !!!
 		
 		
 		
-		return 0;
+		
 		
 	}
 	
@@ -37,12 +139,39 @@ public static int[][] ticbox;
 		//check if a2 is between 0 & 3
 		//check if the selected box is empty ie, already not marked by other player
 		//if all checks passed return true.
+		int f, f1,f2; 
+		if (a1 < 3 && a1 >= 0 )
+		{
+			f = 1;
+		}
+		else 
+			f = 0;
+		if (a2 < 3 && a2 >=0 )
+		{
+			f1 = 1;
+		}
+		else 
+			f1 = 0;
+		if(f == 1 && f1 == 1)
+		{
+		if (tic[a1][a2] == 0)
+		{
+			return true;
+		}
+		else 
+			return false;
+		}
 		
+		/*if (f == 1 && f2 == 1 && f1 ==1)
+		{
+			return true;
+		}
 		
+
 		//Write your code here !!!
 		
-		
-		return false;
+		*/
+		return false;  
 	}
 	public static void main(String args[]) {
 		
@@ -66,6 +195,7 @@ public static int[][] ticbox;
 				int a1 = sc.nextInt();
 				int a2 = sc.nextInt();
 				boolean valid = validate(a1,a2,ticbox);
+				
 				if(valid) {
 					ticbox[a1][a2] = 1;
 					chk = win(ticbox);
@@ -75,13 +205,16 @@ public static int[][] ticbox;
 				else {
 					System.out.println("Please enter a valid position!!");
 				}
-			}else {
+				//sc.close();
+			}
+			else {
 				System.out.println("Player 2 Enter the box number");
 				printBox(ticbox);
 				Scanner sc = new Scanner(System.in);
 				int a1 = sc.nextInt();
 				int a2 = sc.nextInt();
 				boolean valid = validate(a1,a2,ticbox);
+				
 				if(valid) {
 				ticbox[a1][a2] = 2;
 				chk = win(ticbox);
@@ -91,6 +224,7 @@ public static int[][] ticbox;
 				else {
 					System.out.println("Please enter a valid position!!");
 				}
+				//sc.close();
 			}
 			
 			
